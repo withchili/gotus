3ef92ef46b92dc1f49ed0d2f32075259c8d6cc55
withchili <sergei696c696e@gmail.com>
2025-04-28 15:30:34 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,46 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	if i, ok := l.items[key]; ok {
+		i.Value = value
+		l.queue.MoveToFront(i)
+		return true
+	}
+	l.items[key] = l.queue.PushFront(value)
+	if l.queue.Len() > l.capacity {
+		tail := l.queue.Back()
+		if tail != nil {
+			l.queue.Remove(tail)
+			for k, v := range l.items {
+				if v == tail {
+					delete(l.items, k)
+					break
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	if i, ok := l.items[key]; ok {
+		l.queue.MoveToFront(i)
+		return i.Value, true
+	}
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.items = make(map[Key]*ListItem)
+	for l.queue.Len() > 0 {
+		l.queue.Remove(l.queue.Front())
+	}
 }
 
 func NewCache(capacity int) Cache {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,101 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front *ListItem
+	back  *ListItem
+	len   int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	n := ListItem{Value: v, Next: l.front, Prev: nil}
+	if l.len != 0 {
+		l.front.Prev = &n
+	}
+	l.front = &n
+	if l.len == 0 {
+		l.back = &n
+	}
+	l.len++
+	return &n
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	n := ListItem{Value: v, Next: nil, Prev: l.back}
+	if l.len != 0 {
+		l.back.Next = &n
+	}
+	l.back = &n
+	if l.len == 0 {
+		l.front = &n
+	}
+	l.len++
+	return &n
+}
+
+func (l *list) Remove(i *ListItem) {
+	if l.len <= 1 {
+		l.front = nil
+		l.back = nil
+		l.len = 0
+		return
+	}
+	// разбор краевых случаев
+	switch i {
+	case l.front:
+		i.Next.Prev = nil
+		l.front = i.Next
+	case l.back:
+		i.Prev.Next = nil
+		l.back = i.Prev
+	default:
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	}
+	i.Next = nil
+	i.Prev = nil
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if l == nil || i == nil || l.len == 0 || i == l.front {
+		return
+	}
+	if i.Prev != nil {
+		// если не первый
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		// если не последний
+		i.Next.Prev = i.Prev
+	}
+	if l.back == i {
+		// если последний
+		l.back = i.Prev
+	}
+	i.Prev = nil
+	i.Next = l.front
+	if l.front != nil {
+		l.front.Prev = i
+	}
+	l.front = i
 }
 
 func NewList() List {
